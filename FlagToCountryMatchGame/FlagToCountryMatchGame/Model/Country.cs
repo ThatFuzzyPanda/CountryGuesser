9e0df6e3ee57303d820682256c7ee594dcf0a83f
Josh <Silentbutbviolet011@gmail.com>
2024-04-07 11:49:27 -0400

    init commit

---
+++ FlagToCountryMatchGame/FlagToCountryMatchGame/Model/Country.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlagToCountryMatchGame.Model
+{
+
+	public class Countries
+	{
+		public Flags flags { get; set; }
+		public Name name { get; set; }
+		public string[] capital { get; set; }
+	}
+
+	public class Flags
+	{
+		public string png { get; set; }
+		public string svg { get; set; }
+		public string alt { get; set; }
+	}
+
+	public class Name
+	{
+		public string common { get; set; }
+		public string official { get; set; }
+		public Nativename nativeName { get; set; }
+	}
+
+	public class Nativename
+	{
+		public Ell ell { get; set; }
+		public Tur tur { get; set; }
+	}
+
+	public class Ell
+	{
+		public string official { get; set; }
+		public string common { get; set; }
+	}
+
+	public class Tur
+	{
+		public string official { get; set; }
+		public string common { get; set; }
+	}
+
+}
---
+++ FlagToCountryMatchGame/FlagToCountryMatchGame/Views/GameScreen.xaml.cs
@@ -0,0 +1,186 @@
+﻿using FlagToCountryMatchGame.Model;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Timers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FlagToCountryMatchGame.Views
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class GameScreen : Page
+	{
+		
+		int guessCount = 0;
+		int incorrectGuessCount = 0;
+		int start = 60;
+		public static Countries[] countries { get; private set; }
+		public static string GameType;
+		public GameScreen()
+		{
+			this.InitializeComponent();
+			GetData();
+		}
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			GameType = e.Parameter.ToString();
+		}
+
+		void SetText(string Name)
+		{
+			CountryName.Text = Name;
+		}
+		void SetImage(string Path)
+		{
+			string imagePath = Path;
+
+			// Create a new BitmapImage and set its URI source
+			BitmapImage bitmapImage = new BitmapImage(new Uri(imagePath, UriKind.Absolute));
+
+			// Set the Source property of the Image control to the BitmapImage
+			Flag.Source = bitmapImage;
+		}
+		int SetRandomNum()
+		{
+			Random random = new Random();
+			return random.Next(0,countries.Length);
+		}
+		void SetCaptial(string Name)
+		{
+			Captial.Text = Name;
+			Captial.Visibility = Visibility.Visible;
+		}
+		void SetHint(string Name)
+		{
+			Hint.Text = $"Hint: The Capital City: {Name}" ;
+		}
+
+		void SetTime()
+		{
+			
+			
+
+
+		}
+		
+
+		void GameLoop()
+		{
+			int ran = SetRandomNum();
+			CountryName.Visibility = Visibility.Collapsed;
+			CorrectIncorrect.Visibility = Visibility.Collapsed;
+			Hint.Visibility = Visibility.Collapsed;
+			NextBtn.IsEnabled = false;
+			Guess.IsEnabled = true;
+			HintBtn.IsEnabled = true;
+			CorrectGuesses.Text = $"Correct Guesses: {guessCount}";
+			incorrectGuesses.Text = $"Incorrect Guesses: {incorrectGuessCount}";
+			if(GameType == "Flag")
+			{
+				SetText(countries[ran].name.common);
+				SetImage(countries[ran].flags.png);
+				if(countries[ran].capital.Length < 1)
+				{
+					SetHint("Doesnt Have One");
+				}
+				else
+				{
+					SetHint(countries[ran].capital[0]);
+				}
+				
+			}
+			else if (GameType == "FlagTime")
+			{
+				SetTime();
+				SetText(countries[ran].name.common);
+				SetImage(countries[ran].flags.png);
+				if (countries[ran].capital.Length < 1)
+				{
+					SetHint("Doesnt Have One");
+				}
+				else
+				{
+					SetHint(countries[ran].capital[0]);
+				}
+
+			}
+			else
+			{
+				if (countries[ran].capital.Length < 1)
+				{
+					GameLoop();
+				}
+				else
+				{
+					SetText(countries[ran].name.common);
+					SetCaptial(countries[ran].capital[0]);
+				}
+			}
+		}
+
+
+		private async void GetData()
+		{
+			
+			string url = "https://restcountries.com/v3.1/all?fields=name,flags,capital";
+
+			HttpClient client = new HttpClient();
+
+			string responce = await client.GetStringAsync(url);
+			
+			countries = JsonConvert.DeserializeObject<Countries[]>(responce);
+			GameLoop();
+		}
+
+		
+		private void NextBtnClicked(object sender, RoutedEventArgs e)
+		{
+			GameLoop();
+			Guess.Text = "";
+		}
+
+		private void GuesBtnClicked(object sender, RoutedEventArgs e)
+		{
+			CountryName.Visibility = Visibility.Visible;
+			NextBtn.IsEnabled = true;
+			HintBtn.IsEnabled = false;
+			if (Guess.Text.ToLower() == CountryName.Text.ToLower())
+			{
+				guessCount++;
+				
+				CorrectGuesses.Text = $"Correct Guesses: {guessCount}";
+				incorrectGuesses.Text = $"Incorrect Guesses: {incorrectGuessCount}";
+				CorrectIncorrect.Text = "Correct!";
+				CorrectIncorrect.Visibility = Visibility.Visible;
+			}
+			else
+			{
+				CorrectIncorrect.Text = "Incorrect!";
+				incorrectGuessCount++;
+				CorrectIncorrect.Visibility = Visibility.Visible;
+				Guess.IsEnabled = false;
+			}
+		}
+
+		private void BackBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Frame.Navigate(typeof(MainPage), null);
+		}
+		private void HintBtnClicked(object sender, RoutedEventArgs e)
+		{
+			Hint.Visibility = Visibility.Visible;
+		}
+	}
+}
